2ff4160a75dbcef758a6e4ba5a2b02f138474ec1
David Enciso <david.enciso@g-global.com>
2023-06-07 15:24:31 -0600

    ajuste en models

--- models/models.go
+++ models/models.go
@@ -1,7 +1,5 @@
 package models
 
-import "github.com/jinzhu/gorm"
-
 type SecretRDS struct {
 	Username            string `json:"username"`
 	Password            string `json:"password"`
@@ -14,10 +12,4 @@ type SecretRDS struct {
 type SignUp struct {
 	UserEmail string `json:"UserEmail"`
 	UserUUID  string `json:"UserUUID"`
-}
-
-type User struct {
-	gorm.Model
-	Uuid  string `json:"uuid"`
-	Email string `json:"email" gorm:index`
 }
